1313422af516a75ee0bfbf78bda4eea1cae81808
Michael Ballard <ballard.michael9@gmail.com>
2019-10-08 15:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 
 function findMatching(collection, string){
-    collection.filter(item => item.toLowerCase() === string.toLowerCase());
+    return collection.filter(item => item.toLowerCase() === string.toLowerCase());
 }
