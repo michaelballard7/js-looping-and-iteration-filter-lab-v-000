2c11e7550ea0dbb3c2da16918fc7e8c06a044ee2
Michael Ballard <ballard.michael9@gmail.com>
2019-10-08 15:42:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 function findMatching(collection, string){
     return collection.filter(item => item.toLowerCase() === string.toLowerCase());
 };
+
+
+function fuzzyMatch(collection, string){
+    return collection.filter((item)=>{
+        if(item.splice(string.length,0) === string){
+            return 
+        }
+    })
+}
