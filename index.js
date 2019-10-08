c708e4165c4d68901916e6e55da9a63fccde26cb
Michael Ballard <ballard.michael9@gmail.com>
2019-10-08 15:29:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file
+
+let findMatching = drivers.filter((driver, string)=>{
+    if(driver.name.toLowerCase() === string.toLowerCase()){
+        return driver.name
+    }
+})
